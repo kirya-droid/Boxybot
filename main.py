da63bfba988b19a289881f57feb6d80a374763da
kirya-droid <kirya54@gmail.com>
2021-07-13 21:58:34 +0300

    vlad
    vlad

--- main.py
+++ main.py
@@ -2,49 +2,31 @@
 import telebot
 
 bot = telebot.TeleBot('1869364233:AAEWyxbwOzhGI2vWe5HWhMrkavcyaQnvbcI')
-with open("Preparat.txt", encoding="utf-8") as file:
+with open("History.txt", encoding="utf-8") as file:
     history = file.read()
-with open("kak_prinimat.txt", encoding="utf-8" ) as file:
-    kak_prinimat = file.read()
+f = open("KetoCookbook.pdf","rb")
 
-Protivopokazaniya = "El medicamento no tiene contraindicaciones. Es aceptada incluso por las madres lactantes "
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 
-    bot.reply_to(message, f'Hola, me llamo María y te ayudaré a perder peso. Es un placer conocerte  {message.from_user.first_name} ')
+    bot.reply_to(message, f'Hello, my name is Maria, and I will help you lose weight. Pleased to meet you {message.from_user.first_name} ')
 
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('Si')
-    msg = bot.send_message(message.chat.id, text='¿Quiere saber más sobre el producto? ', reply_markup=keyboard)
+    keyboard.row('Yes please, tell me about yourself and how do I lose weight? ')
+    msg = bot.send_message(message.chat.id, text='Do you want to know a little bit about me?', reply_markup=keyboard)
 
 
 @bot.message_handler(content_types = ['text'])
 def step1(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row("Ver mi instagram🥰")
-    keyboard.row("Obtener un enlace al producto👀")
-    keyboard.row("Dígame, ¿cómo se toma estas gotas? ")
-    keyboard.row("¿Hay alguna contraindicación?")
-    keyboard.row("¿Cuál es la composición de las gotas?")
-    keyboard.row("Ventajas de las gotas ?")
-    keyboard.row("Los resulatados  de  algunos de mis suscriptores")
-
-    if message.text == "Si":
+    keyboard.row('I want to learn more and get the recipe book')
+    if message.text == "Yes please, tell me about yourself and how do I lose weight?":
         msg = bot.send_message(message.chat.id, text=history, reply_markup=keyboard)
+        msg = bot.send_message(message.chat.id, text="To learn more and get your free recipe book, watch the video at the link below.", reply_markup=keyboard)
+    if message.text == "I want to learn more and get the recipe book":
+        msg = bot.send_message(message.chat.id, text="https://i-am.tel/bn4ge", reply_markup=keyboard)
+        bot.send_document(message.chat.id, f)
 
-    if message.text == "Ver mi instagram🥰":
-        msg = bot.send_message(message.chat.id, text="https://www.instagram.com/mariabenitez_fit", reply_markup=keyboard)
-    if message.text == "Obtener un enlace al producto👀":
-        msg = bot.send_message(message.chat.id, text="https://linktr.ee/fitre", reply_markup=keyboard)
-    if message.text == "Dígame, ¿cómo se toma estas gotas?":
-        msg = bot.send_photo(message.chat.id,open('Kak_prinimat.jpg', 'rb'))
-    if message.text == "¿Hay alguna contraindicación?":
-        msg = bot.send_message(message.chat.id, text=Protivopokazaniya, reply_markup=keyboard)
-    if message.text == "¿Cuál es la composición de las gotas?":
-         msg = bot.send_photo(message.chat.id,open('sostav.jpg', 'rb'))
-    if message.text == "Ventajas de las gotas ?":
-        msg = bot.send_photo(message.chat.id, open('Preemushestva.jpg', 'rb'))
-    if message.text == "Los resulatados  de  algunos de mis suscriptores":
-        msg = bot.send_photo(message.chat.id, open('rezultat.jpg', 'rb'))
+
 bot.polling(none_stop=True)
